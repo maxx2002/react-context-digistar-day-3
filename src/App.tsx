7a8104da668ab0c1340238a7d9a7f8e255ab9bde
maxx2002 <max.wiranata@unitcode.org>
2024-08-30 15:53:58 +0700

    init: initialize portfolio project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import ProjectDetail from "./pages/ProjectDetail";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="bg-gray-900 px-16 py-6 flex justify-between items-center">
+        <img src="/images/logo.png" alt="Logo" className="h-6 mr-4" />
+        <nav className="flex space-x-8">
+          <Link to="/" className="text-white text-lg">
+            Home
+          </Link>
+          <Link to="/projects" className="text-white text-lg">
+            Projects
+          </Link>
+        </nav>
+      </nav>
+      <main className="bg-white min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:projectId" element={<ProjectDetail />} />
+        </Routes>
+      </main>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { projects } from "../data/projects";
+import ProjectCard from "../components/ProjectCard";
+
+const Projects: React.FC = () => {
+  return (
+    <div className="px-16 py-8 text-gray-900 bg-gray-200 min-h-screen">
+      <h1 className="text-4xl font-bold mb-8">Projects</h1>
+      <div className=" flex gap-x-8">
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            id={project.id}
+            name={project.name}
+            image={project.image}
+            techStack={project.techStack}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
